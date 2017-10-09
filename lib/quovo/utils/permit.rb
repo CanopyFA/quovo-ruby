2d4a249536ca350854448dbc2fd78d789d7cc939
Christopher Ostrowski <chris@madebyfunction.com>
2017-10-09 14:23:58 -0400

    Fixed permit module.

--- lib/quovo/utils/permit.rb
+++ lib/quovo/utils/permit.rb
@@ -1,6 +1,10 @@
 module Quovo
   module Utils
     module Permit
+      def permit!(hash, *keys)
+        Quovo::Utils::Permit.permit!(hash, *keys)
+      end
+
       def self.permit!(hash, *keys)
         hash.delete_if { |k, _| !keys.include?(k) }
       end
